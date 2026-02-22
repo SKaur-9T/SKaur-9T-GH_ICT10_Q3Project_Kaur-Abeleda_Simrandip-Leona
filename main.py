9f04fa59a5d33ba7f705ea3b86b64d0882f5817c
SKaur-9T <kaur.si@obmontessori.edu.ph>
2026-02-22 18:46:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# Account Creation Skill Test
+from pyscript import display, document
+
+
+def account_authentication(event):
+
+    # Clear previous output
+    document.getElementById("output").innerHTML = ""
+
+    username = document.getElementById("input1").value
+    password = document.getElementById("input2").value
+
+    # Validation Conditions
+    if len(username) < 7 and len(password) < 10:
+        display("Username must be at least 7 characters AND password must be at least 10 characters.", target="output")
+
+    elif len(username) < 7:
+        display("Username must be at least 7 characters.", target="output")
+
+    elif len(password) < 10:
+        display("Password must be at least 10 characters.", target="output")
+
+    elif password.isalpha():
+        display("Password must contain at least one number.", target="output")
+
+    elif password.isdigit():
+        display("Password must contain at least one letter.", target="output")
+
+    else:
+        display("✅ Account successfully created! Your username and password are valid.", target="output")
